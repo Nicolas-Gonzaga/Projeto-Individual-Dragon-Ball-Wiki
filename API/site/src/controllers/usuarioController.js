7921a5b097471ac4b5ea256281070a80bc7eb397
Nicolas-Gonzaga <nicolas.gonzaga@sptech.school>
2022-05-25 09:42:22 -0300

    Configuração da API do cadastro

--- API/site/src/controllers/usuarioController.js
+++ API/site/src/controllers/usuarioController.js
@@ -65,6 +65,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var transformacao = req.body.transfServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -73,8 +74,9 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else {
-        
+    } else if (transformacao == undefined){
+        res.status(400).send("Sua senha está undefined!");
+    }else{
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nome, email, senha)
             .then(

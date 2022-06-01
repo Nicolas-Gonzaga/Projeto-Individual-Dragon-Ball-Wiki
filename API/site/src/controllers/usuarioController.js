762e7d0a7059aebb8646e178196052752ed304d6
Nicolas-Gonzaga <nicolas.gonzaga@sptech.school>
2022-06-01 16:02:49 -0300

    Banco no bloco de ntoas

--- API/site/src/controllers/usuarioController.js
+++ API/site/src/controllers/usuarioController.js
@@ -76,7 +76,7 @@ function cadastrar(req, res) {
         res.status(400).send("Sua senha está undefined!");
     } else if (transformacao == undefined){
         res.status(400).send("Sua transformacao está undefined!");
-    }else{
+    } else{
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nome, email, senha, transformacao)
             .then(

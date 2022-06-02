a42d136148d86fec89c910455248c54e8edf1a9a
Nicolas-Gonzaga <nicolas.gonzaga@sptech.school>
2022-06-02 15:39:16 -0300

    Dashboard dinâmica

--- API/site/src/models/medidaModel.js
+++ API/site/src/models/medidaModel.js
@@ -14,14 +14,9 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
                     where fk_aquario = ${idAquario}
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = ` select transformacao.NomeTransformacao, count(usuario.idUsuario) as 'qtd_pessoas'from
+        usuario join transformacao
+        on usuario.fkTransformacao = Transformacao.idTransformacao group by Transformacao.NomeTransformacao;`
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -45,13 +40,9 @@ function buscarMedidasEmTempoReal(idAquario) {
                     order by id desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+        instrucaoSql = ` select transformacao.NomeTransformacao, count(usuario.idUsuario) qtd_pessoas from
+        usuario join transformacao
+        on usuario.fkTransformacao = Transformacao.idTransformacao group by Transformacao.NomeTransformacao;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
